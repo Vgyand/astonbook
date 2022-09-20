26c4784d8295bc10e1f2c8bff753d6e000a02b38
Vgyand <gyandzhunczevv@gmail.com>
2022-09-20 10:34:52 +0300

    ProjectInit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+  poweredByHeader: false,
+  optimizeFonts: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5001/api/:path*",
+      },
+      {
+        source: "/uploads/:path*",
+        destination: "http://localhost:5001/uploads/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
